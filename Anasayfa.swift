434456f4ddb85a11e9d217d1a15470dd78c234ad
Alper Canimoglu <teknoaydin24@gmail.com>
2024-04-07 22:43:10 +0300

    Functions
    Functions are created on ViewModel.

--- Anasayfa.swift
+++ Anasayfa.swift
@@ -13,27 +13,25 @@ class Anasayfa: UIViewController {
     @IBOutlet weak var number1Textfield: UITextField!
     @IBOutlet weak var number2Textfield: UITextField!
     
+    var viewModel = AnasayfaViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        resultLabel.text = "0"
+        resultLabel.text = viewModel.resultVM
     }
     
     @IBAction func toplaButtonClicked(_ sender: Any) {
         if let number1 = number1Textfield.text, let number2 = number2Textfield.text {
-            if let number1Int = Int(number1), let number2Int = Int(number2) {
-                let result = number1Int + number2Int
-                resultLabel.text = String(result)
-            }
+            viewModel.topla(number1: number1, number2: number2) // calistir
+            resultLabel.text = viewModel.resultVM // oku
         }
     }
     
     @IBAction func carpButtonClicked(_ sender: Any) {
         if let number1 = number1Textfield.text, let number2 = number2Textfield.text {
-            if let number1Int = Int(number1), let number2Int = Int(number2) {
-                let result = number1Int * number2Int
-                resultLabel.text = String(result)
-            }
+            viewModel.carpma(number1: number1, number2: number2) // calistir
+            resultLabel.text = viewModel.resultVM // oku
         }
     }
 }
--- AnasayfaViewModel.swift
+++ AnasayfaViewModel.swift
@@ -7,4 +7,21 @@
 
 import Foundation
 
-
+class AnasayfaViewModel {
+    
+    var resultVM = "0"
+    
+    func topla(number1: String, number2: String){
+        if let number1Int = Int(number1), let number2Int = Int(number2) {
+            let result = number1Int + number2Int
+            resultVM = String(result)
+        }
+    }
+    
+    func carpma(number1: String, number2: String){
+        if let number1Int = Int(number1), let number2Int = Int(number2) {
+            let result = number1Int * number2Int
+            resultVM = String(result)
+        }
+    }
+}
