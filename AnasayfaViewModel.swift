e990fb4d46abbcfbffc0713f878583600e5dc838
Alper Canimoglu <teknoaydin24@gmail.com>
2024-04-09 19:29:30 +0300

    RxSwift
    RxSwift

---
+++ AnasayfaViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  AnasayfaViewModel.swift
+//  MVVM-Giris
+//
+//  Created by Alper Canımoğlu on 7.04.2024.
+//
+
+import Foundation
+import RxSwift
+
+class AnasayfaViewModel {
+    
+    var resultVM = BehaviorSubject<String>(value: "0")
+    
+    func topla(number1: String, number2: String){
+        if let number1Int = Int(number1), let number2Int = Int(number2) {
+            let result = number1Int + number2Int
+            resultVM.onNext(String(result)) // (1) tetikleme
+        }
+    }
+    
+    func carpma(number1: String, number2: String){
+        if let number1Int = Int(number1), let number2Int = Int(number2) {
+            let result = number1Int * number2Int
+            resultVM.onNext(String(result)) // (1) tetikleme
+        }
+    }
+}
